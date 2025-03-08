c3f419111eef28525d2f73618614edc9e5a2f8fc
Prince2531 <kumarprince2503@gmail.com>
2025-03-08 23:43:19 +0530

    DAY : 2 of 100 days coding challenge

--- prince.java
+++ prince.java
@@ -1,8 +1,8 @@
 class prince {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = a+b;
+        int a = 20;
+        int b = 10;
+        int c = a - b;
         System.out.println(c);
     }
 }
